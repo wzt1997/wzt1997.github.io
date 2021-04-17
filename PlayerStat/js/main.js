fe7107f99020721fce40362bf5dc71521a951c04
wzt1997 <wzt19971222@gmail.com>
2021-04-17 23:03:14 +0800

    add index

--- PlayerStat/js/main.js
+++
@@ -1,9 +0,0 @@
-let svg = d3.select('#mainsvg');
-let maingroup = svg
-.append('g')
-.attr('transform', `translate(${300}, ${150})`)
-let circle = maingroup
-.append('circle')
-.attr('r', '100')
-.attr('fill', 'green')
-.attr('stroke', 'black')
